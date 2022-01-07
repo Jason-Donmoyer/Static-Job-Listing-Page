6aeb1a2f87db86b8f503dd2e293064aff66d569f
Jason-Donmoyer <j.donmoyer523@gmail.com>
2022-01-07 17:56:14 -0500

    HTML, CSS, JS: -HTML: --removed static files and placed into static.html -CSS: --reset.css complete --started styles for header and job listing container --set up variables -JS: --added comment

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
+// array to hold job listing information
 const jobListings = [];
 
+
+// Fetch API call to extract json data
 fetch('../data.json')
   .then(response => {
-    
     return response.json();
   })
   .then(result => {
     for(let i in result) {
       let jobListing = {};
-      console.log(result[i].logo);
+      // console.log(result[i].tools);
       jobListing.logo = result[i].logo;
+      jobListing.companyName = result[i].company;
+      jobListing.isNew = result[i].new;
+      jobListing.isFeatured = result[i].featured;
+      jobListing.position = result[i].position;
       jobListings.push(jobListing);
     }
   })
   .catch(err => {
     console.log(err);
   });
+
+  console.log(jobListings);
 
