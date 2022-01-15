9ba207e5458b8a488dfdda97024642a5cf377312
Jason-Donmoyer <j.donmoyer523@gmail.com>
2022-01-15 06:05:25 -0500

    HTML, CSS and JS: -HTML: --removed hard coded job listings -CSS: --fixed issue with content wrapping -JS: --finished dynamic job listing function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ const jobListings = [];
 
 const mainContainer = document.getElementById('main-container');
 
+
+function makeCircle(par) {
+  let circle = document.createElement('div');
+  circle.classList.add('small-circle');
+  par.appendChild(circle);
+}
+
 function updateUI(jobListings) {
   
   jobListings.forEach((listing) => {
@@ -21,7 +28,7 @@ function updateUI(jobListings) {
   let jobListingDescriptionContainer = document.createElement('div');
     jobListingDescriptionContainer.classList.add('job-listing-description-container');
     
-    jobListing.appendChild(jobListingDescriptionContainer);
+    desktopListing.appendChild(jobListingDescriptionContainer);
   let jobListingFeaturesContainer = document.createElement('div');
     jobListingFeaturesContainer.classList.add('job-listing-features-container');
     
@@ -38,39 +45,78 @@ function updateUI(jobListings) {
 
   if (listing.isNew) {
     let newListing = document.createElement('div');
-    newListing.classList.add('listing-container');
-    newListing.classList.add('new-listing-container');
+      newListing.classList.add('listing-container');
+      newListing.classList.add('new-listing-container');
     let listing = document.createElement('h3');
-    listing.classList.add('listing');
-    listing.textContent = 'NEW!';
-    newListing.appendChild(listing);
+      listing.classList.add('listing');
+      listing.textContent = 'NEW!';
+      newListing.appendChild(listing);
 
     jobListingFeaturesContainer.appendChild(newListing);
   }
 
   if (listing.isFeatured) {
     let newListing = document.createElement('div');
-    newListing.classList.add('listing-container');
-    newListing.classList.add('featured-listing-container');
+      newListing.classList.add('listing-container');
+      newListing.classList.add('featured-listing-container');
     let listing = document.createElement('h3');
-    listing.classList.add('listing');
-    listing.textContent = 'FEATURED!';
-    newListing.appendChild(listing);
-    jobListing.classList.add('featured-listing-border');
+      listing.classList.add('listing');
+      listing.textContent = 'FEATURED!';
+      newListing.appendChild(listing);
+      jobListing.classList.add('featured-listing-border');
 
     jobListingFeaturesContainer.appendChild(newListing);
   }
 
   let jobTitle = document.createElement('h1');
-  jobTitle.classList.add('job-title');
-  jobTitle.textContent = listing.position;
-  jobListingDescriptionContainer.appendChild(jobTitle);
+    jobTitle.classList.add('job-title');
+    jobTitle.textContent = listing.position;
+    jobListingDescriptionContainer.appendChild(jobTitle);
   
   let jobListingInfoContainer = document.createElement('div');
-  jobListingInfoContainer.classList.add('job-listing-info-container');
-  jobListingDescriptionContainer.appendChild(jobListingInfoContainer);
+    jobListingInfoContainer.classList.add('job-listing-info-container');
+    jobListingDescriptionContainer.appendChild(jobListingInfoContainer);
 
+  let jobRole = document.createElement('h4');
+    jobRole.classList.add('job-info');
+    jobRole.id = 'role';
+    jobRole.textContent = listing.role;
+    jobListingInfoContainer.appendChild(jobRole);
 
+  makeCircle(jobListingInfoContainer);
+
+  let jobLevel = document.createElement('h4');
+    jobLevel.classList.add('job-info');
+    jobLevel.id = 'level';
+    jobLevel.textContent = listing.level;
+    jobListingInfoContainer.appendChild(jobLevel);
+
+  makeCircle(jobListingInfoContainer);
+  
+  let jobPostedAt = document.createElement('h4');
+    jobPostedAt.classList.add('job-info');
+    jobPostedAt.id = 'posted-at';
+    jobPostedAt.textContent = listing.postedAt;
+    jobListingInfoContainer.appendChild(jobPostedAt);
+
+  let horizontalLine = document.createElement('div');
+    horizontalLine.classList.add('horizontal-line');
+    jobListingDescriptionContainer.appendChild(horizontalLine);
+
+  let jobListingFiltersContainer = document.createElement('div');
+    jobListingFiltersContainer.classList.add('job-listing-filters-container')
+    desktopListing.appendChild(jobListingFiltersContainer);
+
+    listing.filters.forEach((filter) => {
+      let filterBtn = document.createElement('div');
+      filterBtn.classList.add('filter-btn');
+      filterBtn.classList.add('job-listing-filter-btn');
+      let filterBtnCopy = document.createElement('p');
+      filterBtnCopy.classList.add('filter-btn-copy');
+      filterBtnCopy.textContent = filter;
+      filterBtn.appendChild(filterBtnCopy);
+      jobListingFiltersContainer.appendChild(filterBtn);
+    });
 
   mainContainer.appendChild(jobListing);
 
@@ -91,6 +137,10 @@ async function getJobListings() {
       jobListing.isNew = result[i].new;
       jobListing.isFeatured = result[i].featured;
       jobListing.position = result[i].position;
+      jobListing.role = result[i].role;
+      jobListing.level = result[i].level;
+      jobListing.postedAt = result[i].postedAt;
+      jobListing.filters = result[i].languages.concat(result[i].tools);
       jobListings.push(jobListing);
     }
     updateUI(jobListings);
@@ -109,60 +159,4 @@ async function getJobListings() {
 getJobListings();
 
   
-
-{/* <article class="job-listing-container featured-listing-border">
-    
-
-    <div class="company-logo"></div>
-    <div class="job-listing-desktop-container">
-    <div class="job-listing-description-container">
-      <div class="job-listing-features-container">
-        <h2 class="company-name">Photosnap</h2>
-        <div class="features-container">
-          <div class="listing-container new-listing-container">
-            <h3 class="listing">New!</h3>
-          </div>
-          <div class="listing-container featured-listing-container">
-            <h3 class="listing">Featured</h3>
-          </div>
-        </div>
-      </div>
-      
-      <h1 class="job-title">Senior Frontend Developer</h1>
-      
-      <div class="job-listing-info-container">
-        <h4 class="job-info">1d ago</h4> 
-        <div class="small-circle"></div>
-        <h4 class="job-info">Full Time</h4>
-        <div class="small-circle"></div>
-        <h4 class="job-info">USA only</h4>
-      </div>
-      
-    </div>
-    
-    <div class="horizontal-line"></div>
-
-    <div class="job-listing-filters-container">
-      <!-- Role -->
-      <div class="filter-btn job-listing-filter-btn">
-        <p class="filter-btn-copy">Frontend</p>
-      </div>
-      
-      <!-- Level -->
-      <div class="filter-btn job-listing-filter-btn">
-        <p class="filter-btn-copy">Senior</p>
-      </div>
-      
-      <!-- Languages -->
-      <div class="filter-btn job-listing-filter-btn">
-        <p class="filter-btn-copy">HTML</p>
-      </div>
-
-      
-      
-    </div>
-  </div>
-
-  </article> */}
-
 
